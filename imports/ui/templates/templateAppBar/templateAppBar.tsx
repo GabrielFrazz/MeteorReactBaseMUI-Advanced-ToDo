39ac3e1df10ebad2db05b468d3bf8ec424ce426a
GabrielFrazz <carlos.gofrazao@gmail.com>
2025-04-29 11:45:00 -0300

    mudanças na appbar

--- imports/ui/templates/templateAppBar/templateAppBar.tsx
+++ imports/ui/templates/templateAppBar/templateAppBar.tsx
@@ -1,43 +1,33 @@
 import React from 'react';
-import {ISysTemplateProps} from '../getTemplate';
-import {BoxProps, Typography} from '@mui/material';
+import { ISysTemplateProps } from '../getTemplate';
+import { BoxProps, Typography, Box } from '@mui/material';
 import TemplateAppBarStyles from './templateAppBarStyles';
 import SysAppBar from '../components/sysAppBar/sysAppBarController';
 
 export interface ITemplateAppBar extends ISysTemplateProps {
-  containerProps?: BoxProps;
-  logo?: React.ReactNode;
+	containerProps?: BoxProps;
+	logo?: React.ReactNode;
 }
 
-export const TemplateAppBar: React.FC<ITemplateAppBar> = ({
-  children,
-  menuOptions,
-  logo,
-  containerProps
-}) => {
-  return (
-    <TemplateAppBarStyles.container>
-      <SysAppBar logo={logo ?? <BoilerplateLogo />} menuOptions={menuOptions} />
-      <TemplateAppBarStyles.contentWrapper>
-        <TemplateAppBarStyles.contentContainer {...containerProps}>
-          {children}
-        </TemplateAppBarStyles.contentContainer>
-      </TemplateAppBarStyles.contentWrapper>
-    </TemplateAppBarStyles.container>
-  );
+export const TemplateAppBar: React.FC<ITemplateAppBar> = ({ children, menuOptions, logo, containerProps }) => {
+	return (
+		<TemplateAppBarStyles.container>
+			<SysAppBar logo={logo ?? <BoilerplateLogo />} menuOptions={menuOptions} />
+			<TemplateAppBarStyles.contentWrapper>
+				<TemplateAppBarStyles.contentContainer {...containerProps}>{children}</TemplateAppBarStyles.contentContainer>
+			</TemplateAppBarStyles.contentWrapper>
+		</TemplateAppBarStyles.container>
+	);
 };
 
 const BoilerplateLogo: React.FC = () => {
 	return (
 		<Typography
-			variant="subtitle1"
-			color={(theme) => theme.palette.sysText?.tertiary}
-			sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
-			{'{ '}
-			<Typography color={(theme) => theme.palette.primary.contrastText} variant="inherit">
-				Boilerplate
-			</Typography>{' '}
-			{'}'}
+			sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', fontWeight:700 }}
+			color={(theme) => theme.palette.primary.contrastText}
+			variant="inherit">
+			<TemplateAppBarStyles.Logo />
+			Advanced <br /> To-Do
 		</Typography>
 	);
 };
